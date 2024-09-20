014da40f608c931ee5d4fcd602ff9744280fd6cb
Maxim Frolov <max.fro2021@gmail.com>
2024-09-20 11:10:12 +0200

    refactor: Reorganized and commented the control structures in mem_os.h

--- headers/mem_os.h
+++ headers/mem_os.h
@@ -10,11 +10,23 @@
 //include stdlib pour definition du type size_t
 #include <stdlib.h>
 
-//Definie la structure the bloc libre
+//Definie la structure du bloc libre
 typedef struct mem_free_block_s {
-    size_t size ;
-    struct mem_free_block_s* next ;
+    size_t size; // taille du bloc courant
+    struct mem_free_block_s* next; // pointeur vers le bloc memoire libre suivant
 } mem_free_block_t;
+
+//Definie la structure du bloc occupé
+typedef struct mem_busy_block_s {
+    size_t size; // taille du bloc courant
+} mem_busy_block_t;
+
+//Definie la structure du header de la zone memoire
+typedef struct mem_header_s {
+    size_t size; // taille complete de la memoire
+    struct mem_free_block_s* first; // pointeur vers le premier bloc memoire libre
+} mem_header_t;
+
 
 /* -----------------------------------------------*/
 /* Interface de gestion de votre allocateur       */
