e43891fbc66c45251770bb05fe6c2b832d583e28
Culis Gaspard <culisg@im2ag-211-06.u-ga.fr>
2024-09-23 13:18:29 +0200

    refactor(shell.nix): Now using more flexible nixpkgs source

--- shell.nix
+++ shell.nix
@@ -1,14 +1,11 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-
-pkgs.mkShell {
-  packages = with pkgs; [
-    helix
-    clang-tools
-    lldb
-    cmake-language-server
-    bear
-  ];
-}
+{pkgs ? import <nixpkgs> {}}:
+with pkgs;
+  mkShell rec {
+    packages = with pkgs; [
+      helix
+      clang-tools
+      lldb
+      cmake-language-server
+      bear
+    ];
+  }
