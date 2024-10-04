00525b2fe3641818e184c74eaa12e3a2deca6816
Maxim Frolov <max.fro2021@gmail.com>
2024-10-04 08:26:39 +0200

    docs: unified the language of the comments.

--- headers/mem_os.h
+++ headers/mem_os.h
@@ -10,26 +10,24 @@
 // include stdlib pour definition du type size_t
 #include <stdlib.h>
 
-/* -----------------------------------------------*/
-/* Interface de gestion de votre allocateur       */
-/* -----------------------------------------------*/
-// Initialisation
+
+/// Initialisation
 void mem_init(void);
 
 /// Free memory block control struct
 typedef struct mem_free_block_s {
   /// The size of the block, including the size of its control struct (+24)
   size_t size;
-  /// Pointeur vers la structure de controle du prochain bloc memoire libre
+  /// Pointer to the next free block of the memory
   struct mem_free_block_s *next;
-  /// Pointeur vers la structure de controle du bloc de memoire libre précédent
+  /// Pointer to the previous free block of the memory
   struct mem_free_block_s *prev;
 } mem_free_block_t;
 
 /// Type of an allocation fit function signature
 typedef mem_free_block_t *(mem_fit_function_t)(mem_free_block_t *, size_t);
 
-// Definition la structure du bloc occupé
+/// Occupied memory control struct
 typedef struct mem_busy_block_s {
   /// The total size of the occupied block (including control struct and possible extra for sys purposes)
   size_t size;  
@@ -49,13 +47,17 @@ typedef struct mem_header_s {
   mem_fit_function_t *fit_function;
 } mem_header_t;
 
-// Choix de la fonction d'allocation
-// = choix de la stratégie de l'allocation
+/// Choice of the allocation strategy 
 void mem_set_fit_handler(mem_fit_function_t *);
 
-// Stratégies de base (fonctions) d'allocation
+/// First fit strategy
+/// Returns the first free block of sufffiecient size.
 mem_fit_function_t mem_first_fit;
+/// Worst fit strategy
+/// Returns the biggest free block of sufffiecient size.
 mem_fit_function_t mem_worst_fit;
+/// Best fit strategy
+/// Returns the smallest free block of sufffiecient size.
 mem_fit_function_t mem_best_fit;
 
 #endif /* MEM_OS_H */
